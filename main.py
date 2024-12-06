5e03b1dc5ca0ed866bd0a5e0c51a095d491db064
Connor Basile <cbasile3@binghamton.edu>
2024-12-05 23:05:01 -0500

    main.py complete final project

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 import pygame
-from src.controller import Controller  # Import your Controller class
+from src.controller import Controller  
 
 def main():
-    # Initialize pygame
+    
     pygame.init()
 
-    # Set up the screen (width and height of the window)
+    
     screen_width = 800
     screen_height = 600
     screen = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption("Blackjack Game")
 
-    # Load font for text display
-    font = pygame.font.SysFont('ARCADECLASSIC', 24)  # Adjust font size as needed
+    
+    font = pygame.font.SysFont('ARCADECLASSIC', 24)  
 
-    # Create an instance of the GameController
+    
     game_controller = Controller(screen, font)
 
-    # Start the game loop
+    
     game_controller.mainloop()
 
-    # Quit pygame when done
+    
     pygame.quit()
 
 if __name__ == "__main__":
